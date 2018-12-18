379e5fd8a329816e2cb2d70e106faab5a2da92e4
aloctavodia <aloctavodia@gmail.com>
2018-12-18 15:20:20 -0300

    use nglview instead of py3d

--- bomeba0/visualization/view3d.py
+++ bomeba0/visualization/view3d.py
@@ -1,13 +1,11 @@
-import py3Dmol
+import nglview as nv
 
 def _view3d(mol):
     """
-    draft of function to visualize a molecule embedded in a notebook
+    Draft of a function to visualize a molecule embedded in a notebook using nlgview
     """
     pdb_str = mol.dump_pdb('prot', to_file=False)
-    view = py3Dmol.view()
-    view.addModel(pdb_str,'pdb')
-    view.setStyle({'cartoon':{'color':'orange'}})
-    view.zoomTo()
-    return view.show()
+    view = nv.show_text(pdb_str)
+    view.update_cartoon(color='#ed7300')
+    return view
 
